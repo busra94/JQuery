72c7ccf591bff875b5053e0dba5d6304ecaf924f
busra94@ <doganbusra94@gmail.com>
2018-10-23 17:53:17 +0300

    forth commit section8

--- js/js_form.js
+++ js/js_form.js
@@ -17,18 +17,18 @@ $("input:text").blur(function(){
   
     if (textInput.length == 0 || textInput.length < 3){
     $("input:text").css("box-shadow", "0 0 4px #811");
-    $(".warning").css(
-        {
-            "display": "block",
-            "color" : "#811"
+    // $(".feedback").css(
+    //     {
+    //         "display": "block",
+    //         "color" : "#811"
 
-        }
-    );
+    //     }
+    // );
     
-    $(".warning").text("Please enter a name or enter longer name");
+    // $(".feedback").text("Please enter a name or enter longer name");
 }
     else{
-        $(".warning").css("display" , "none");
+        // $(".feedback").css("display" , "none");
         $("input:text").css("box-shadow", "0 0 4px #181");
 }
 });
@@ -80,31 +80,108 @@ $(function(){ /* change event especially useful for checkboxes radio buttons and
     });
 });
 
-$(function(){ /* submit */
-    $("#form").submit(function(){
-        var textarea = $("#message");
-        if(textarea.val().trim() == ""){
-            textarea.css("box-shadow", "0 0 4px #811");
-            event.preventDefault(); /* prevent form from submiting because there is something wrong and we should not data to the server and we must give info to user and prevent the page reload. so we can use this function if we want some default event to stop from execution.   */
+// $(function(){ /* submit textarea*/
+//     $("#form").submit(function(){
+//         var textarea = $("#message");
+//         if(textarea.val().trim() == ""){
+//             textarea.css("box-shadow", "0 0 4px #811");
+//             event.preventDefault(); /* prevent form from submiting because there is something wrong and we should not data to the server and we must give info to user and prevent the page reload. so we can use this function if we want some default event to stop from execution.   */
 
 
-        }else{ 
-            /* form wil be submitted.*/
+//         }else{ 
+//             /* form wil be submitted.*/
 
-        }
-    });
-});
+//         }
+//     });
+// });
 
-$(function(){
-    $("#form").submit(function(event){
+$(function(){ /* submit form */
+
+    var form = $("#form");
+    enableFastFeedback(form);
+
+    form.submit(function(event){
         var name = $("#name").val();
         var password = $("#password").val();
         var message = $("#message").val();
-        var ckecked = $("#checkbox").is(":checked");  /* is(:checked) returns boolean value.  */
-
+        var checked = $("#checkbox").is(":checked")  /* is(:checked) returns boolean value.  */
+    
         validateNameField(name, event); 
+        validateCheckboxField(checked, event);
+        validatePasswordField(password,event);
+        validateMessageField( message,event);
     });
 });
+
+function enableFastFeedback(formElement){
+    var nameInput = formElement.find("#name");
+    var passwordInput = formElement.find("#password");
+    var messageInput = formElement.find("#message");
+    var checkboxInput = formElement.find("#checkbox");
+    
+    nameInput.blur(function(event){
+        var name = $(this).val();
+        validateNameField(name, event);
+
+        if(!isValidName(name)){
+            $(this).css({
+                "box-shadow": "0 0 4px #811",
+                "border": "1px solid #600"});
+        }
+        else{
+            $(this).css({
+                "box-shadow": "0 0 4px #181",
+                "border": "1px solid #181"});
+        }
+    });
+
+    passwordInput.blur(function(event){
+        var password = $(this).val();
+        validatePasswordField(password, event);
+
+        if(!isValidPassword(password)){
+            $(this).css({
+                "box-shadow": "0 0 4px #811",
+                "border": "1px solid #600"});
+        }
+        else{
+            $(this).css({
+                "box-shadow": "0 0 4px #181",
+                "border": "1px solid #181"});
+        }
+    });
+
+    messageInput.blur(function(event){
+        var message = $(this).val();
+        validateMessageField(message, event);
+
+        if(!isValidMessage(message)){
+            $(this).css({
+                "box-shadow": "0 0 4px #811",
+                "border": "1px solid #600"});
+        }
+        else{
+            $(this).css({
+                "box-shadow": "0 0 4px #181",
+                "border": "1px solid #060"});
+        }
+    });
+    checkboxInput.change(function(event){  /* change triggered checkbox changed!!! */
+        var isChecked = $(this).is(":checked"); // .prop("checked")
+       validateCheckboxField(isChecked, event);
+
+        if(!isChecked){
+            $(this).add("label[for='checkbox']").css({  /* do not forget add label element for checkbox! */
+                "box-shadow": "0 0 4px #811",
+                "border": "1px solid #600"});
+        }
+        else{
+            $(this).add("label[for='checkbox']").css({
+                "box-shadow": "0 0 4px #181",
+                "border": "1px solid #181"});
+        }
+    });
+}
 
 function validateNameField(name, event){   /* event for preventdefault() */
     if(!isValidName(name)){
@@ -120,6 +197,51 @@ function isValidName(name){
     return name.length >=3;
 }
 
+function validatePasswordField(password,event){
+    if(!isValidPassword(password)){
+        $("#password-feedback").text("The password should have at least 8 characters and contain number!");
+        event.preventDefault();
+    }
+    else{
+    $("#password-feedback").text("");
+    }      
+}
+function isValidPassword(password){
+    return password.length >=8  && /. *[0-9]. */.test(password);  /* password must contain number, The test() method tests for a match in a string.
+
+    This method returns true if it finds a match, otherwise it returns false. */
+}
+
+function validateMessageField(message,event){
+    if(!isValidMessage(message)){
+        $("#message-feedback").text("Please enter a message");
+        event.preventDefault();
+    }
+    else{
+    $("#message-feedback").text("");
+    }      
+}
+function isValidMessage(message){
+    return message.trim() != "";
+
+}
+
+function validateCheckboxField(isChecked,event){
+    if(!isChecked){
+        $("#checkbox-feedback").text("Please check the checkbox");
+        event.preventDefault();        
+    }
+    else{
+        $("#checkbox-feedback").text("");
+    }
+}
+
+/* !! in $(function): 
+var checked = $("#checkbox");
+---------------------
+function isValidCheckBox(checkbox){
+     return checkbox.is(":checked");   
+---------------------     
 function validateCheckboxField(checkbox,event){
     if(!isValidCheckBox(checkbox)){
         $("#checkbox-feedback").text("Please check the checkbox");
@@ -129,7 +251,49 @@ function validateCheckboxField(checkbox,event){
         $("#checkbox-feedback").text("");
     }
 }
-
-function isValidCheckBox(checkbox){
-    return checkbox.is(":checked");
 }
+ */
+
+ /* AJAX SECTION */
+ 
+ /*when we perform a get request generally we must follow origin policy, which essentially means we can only get request to our server, but public servers apis allow to access their data. otherwise we get an error like: request doesn't come same domain
+ with ajax calls we do not have to reload the page to be able to change it's content. */
+/* $.get(). $.post(), $.ajax(), $.getJSON() ... */
+ $(function(){
+     /*$.load() : The load() method loads data from a server and puts the returned data into the selected element.
+     If no element is matched by the selector — in this case, if the document does not contain an element with id="result" — the Ajax request will not be sent.*/
+     // we retrive js_form file (itself)
+    //  var code = $("#code").load("js/js_form.js"); // where we wanna retrieve data --> path
+    //  console.log(code);
+
+    //  $("#code").load("js/js_form.js", function(response,status){
+    //      if(status == "error")
+    //      alert("Could not find file")
+    //      console.log(response);
+    //  });
+
+ }); 
+
+ $(function(){
+    /* JSON, $.getJSON() */
+    var  flickrApiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?" /* ?jsoncallback=? when we add that we send jsonp request not json request and this necessary for retrieve data from another server  */
+    /* this is also an event */
+    $.getJSON(flickrApiUrl, { /* this call is asynchronous. Request was handled by flickr.com  and it returns data and when request finished,  data handled by the event    */
+        //options
+        tags:"children , babies",
+        tagmode: "any",  /* any: sun or beach, all: sun and beach. */
+        format: "json"
+    }).done(function(data){
+        //success
+        console.log(data);
+        $.each(data.items, function(index, item){ /* iterate over arrays, objects, if we set parameter to object then first parameter should be object(key) second is value */
+            console.log(item); 
+            $("<img>").attr( "src", item.media.m).appendTo("#flickr");
+
+        })   
+    }).fail(function(){
+        //failure
+        alert("Ajax call failed."); 
+    }) 
+
+ });
